7386b9114553589fe18538fa84451acb556c2420
Dimserene <raul0520@gmail.com>
2024-07-30 00:33:55 +0800

    2024/07/29 16:33:47

--- Mods/ModpackUtil/ModpackUtil.lua
+++ Mods/ModpackUtil/ModpackUtil.lua
@@ -27,7 +27,7 @@ local version = nativefs.read(lovely.mod_dir .. "/ModpackUtil/version.txt")
 local updated = os.date("!%Y/%m/%d %H:%M:%S", love.filesystem.getLastModified(lovely.mod_dir .. "/ModpackUtil/version.txt"))
 
 
-local MODPACK_VERSION = "Dimserene's Modpack - Fine-tuned" .. "\nCurrent Version: " .. version .. "     Last Update: " .. updated
+local MODPACK_VERSION = "Dimserene's Modpack - Fine-tuned" .. "\nCurrent Version: " .. version .. "   Game Started: " .. updated
 
 
 local gameMainMenuRef = Game.main_menu
