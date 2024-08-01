c8e3707fd35422f6e50b1295316adbe5da090e35
Dimserene <raul0520@gmail.com>
2024-08-01 19:16:08 +0800

    2024-08-01c

--- pwsh-version.ps1
+++ pwsh-version.ps1
@@ -1,7 +1,8 @@
-xcopy /y .\CurrentVersion.txt .\Mods\ModpackUtil\
-Get-Date -AsUTC -UFormat "%Y/%m/%d %H:%M:%S" > .\Mods\ModpackUtil\VersionTime.txt
 git pull
 git submodule update --remote --recursive --merge
+notepad.exe CurrentVersion.txt
+xcopy /y .\CurrentVersion.txt .\Mods\ModpackUtil\
+Get-Date -AsUTC -UFormat "%Y/%m/%d %H:%M:%S" > .\Mods\ModpackUtil\VersionTime.txt
 git add .
 git commit -F Mods\ModpackUtil\CurrentVersion.txt
 git push
--- sh-version.sh
+++ sh-version.sh
@@ -1,8 +1,8 @@
+git pull
+git submodule update --remote --recursive --merge
 nano ./CurrentVersion.txt
 cp ./CurrentVersion.txt ./Mods/ModpackUtil/
 date -u "+%Y/%m/%d %H:%M:%S" > Mods/ModpackUtil/VersionTime.txt
-git pull
-git submodule update --remote --recursive --merge
 git add .
 git commit -F Mods/ModpackUtil/CurrentVersion.txt
 git push
