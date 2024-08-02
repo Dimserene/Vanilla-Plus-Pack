0ce338a26c61596ef1304b0cb79c7dd6e686d12a
Dimserene <raul0520@gmail.com>
2024-08-02 18:24:15 +0800

    2024-08-02a

--- pwsh-version.ps1
+++ pwsh-version.ps1
@@ -1,8 +1,24 @@
+
+# Update the local repository with the latest changes from the remote repository
 git pull
+
+# Update all submodules to their latest commit, using the merge strategy
 git submodule update --remote --recursive --merge
+
+# Open the CurrentVersion.txt file in Notepad for editing
 notepad.exe CurrentVersion.txt
+
+# Copy the updated CurrentVersion.txt file to the specified directory
 xcopy /y .\CurrentVersion.txt .\Mods\ModpackUtil\
+
+# Create or overwrite VersionTime.txt with the current date and time in UTC format
 Get-Date -AsUTC -UFormat "%Y/%m/%d %H:%M:%S" > .\Mods\ModpackUtil\VersionTime.txt
+
+# Stage all changes for the next commit
 git add .
+
+# Commit the staged changes using the contents of CurrentVersion.txt as the commit message
 git commit -F Mods\ModpackUtil\CurrentVersion.txt
+
+# Push the committed changes to the remote repository
 git push
--- sh-version.sh
+++ sh-version.sh
@@ -1,8 +1,23 @@
+# Update local repository with the latest changes from the remote repository
 git pull
+
+# Update all submodules to their latest commit from the remote, merge them into the current branch
 git submodule update --remote --recursive --merge
+
+# Open the CurrentVersion.txt file for editing in the nano text editor
 nano ./CurrentVersion.txt
+
+# Copy the CurrentVersion.txt file to the Mods/ModpackUtil/ directory
 cp ./CurrentVersion.txt ./Mods/ModpackUtil/
+
+# Write the current UTC date and time to VersionTime.txt in the Mods/ModpackUtil/ directory
 date -u "+%Y/%m/%d %H:%M:%S" > Mods/ModpackUtil/VersionTime.txt
+
+# Stage all changes in the current directory for commit
 git add .
+
+# Commit the staged changes with the message from CurrentVersion.txt
 git commit -F Mods/ModpackUtil/CurrentVersion.txt
+
+# Push the committed changes to the remote repository
 git push
