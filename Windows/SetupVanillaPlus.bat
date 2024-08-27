960317134ac6cbff0c9339b567535d338538b895
Dimserene <raul0520@gmail.com>
2024-08-27 22:27:55 +0800

    2024-08-27a

--- Windows/SetupVanillaPlus.bat
+++ Windows/SetupVanillaPlus.bat
@@ -1,3 +1,14 @@
+@echo off
+echo *******************************************************
+echo IMPORTANT: Please backup your current Mods folder before proceeding.
+echo The installation will overwrite existing files in your Mods directory.
+echo *******************************************************
+pause
+
 git clone --recurse-submodules --remote-submodules https://github.com/Dimserene/Vanilla-Plus-Pack
-xcopy "Dimserenes-Modpack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+
+echo Copying Mods folder to %appdata%\Balatro\Mods
+xcopy "Vanilla-Plus-Pack\Mods" "%appdata%\Balatro\Mods" /w /e /h /i
+
+echo Installation complete. Please ensure everything is working correctly.
 pause
