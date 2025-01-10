f7112e31c6ea9db8cedc7345750bd89ca473ba3b
Dimserene <raul0520@google.com>
2025-01-10 14:18:44 +0800

    2025-01-10b

--- Mods/Steamodded/config.lua
+++
@@ -1,7 +0,0 @@
-return {
-    ["no_mod_badges"] = false,
-    ["graphics_mipmap_level"] = 3,
-    ["graphics_mipmap_level_options"] = {0, 2, 4, 8},
-    ["achievements"] = 1,
-    ["seeded_unlocks"] = false,
-}
